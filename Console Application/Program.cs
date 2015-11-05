3fc59b56ae2addfdab25814e31b3b1b70a21c825
Alessandro Piccione <alex.75@tin.it>
2015-11-05 23:12:37 +0100

    Created solution

---
+++ Console Application/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Application
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("\n\nPress any key to close ");
+			Console.ReadKey();
+
+		}
+	}
+}
