4383a2fa6b0bcb819d6b023d463e0beff6c8d91e
Alessandro Piccione <alex.75@tin.it>
2015-12-06 00:41:49 +0100

    Add tests on connection

---
+++ Tests/Features/Connection.feature
@@ -0,0 +1,19 @@
+﻿Feature: Connection
+	In order to work with the database
+	As a client
+	I want to connect to it
+
+@connection
+Scenario: Connect to test database works
+	Given connection server is "localhost" and port is 27101	
+	And I use the user "alex" with the password "aaa" on the database "admin"	
+	When I try to connect to "test" database
+	Then there are no errors
+
+
+@connection
+Scenario: Connect to test database return error with wrong credentials
+	Given connection server is "localhost" and port is 27101	
+	And I use the user "fake_user" with the password "aaa" on the database "admin"	
+	When I try to connect to "test" database
+	Then return an error
---
+++ Tests/Steps/ConnectionSteps.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using MongoDB.Driver;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Tests.Steps
+{
+    [Binding]
+    public class ConnectionSteps
+    {
+		private MongoClientSettings settings;
+		private MongoClient client;
+		private Exception exception;
+
+		public ConnectionSteps()		{
+
+
+		}
+
+        [Given(@"connection server is ""(.*)"" and port is (.*)")]
+        public void GivenConnectionServerIsAndPortIs(string server, int port)
+        {
+            settings = new MongoClientSettings() {
+				Server = new MongoServerAddress(server, port)
+			};
+        }
+        
+        [Given(@"I use the user ""(.*)"" with the password ""(.*)"" on the database ""(.*)""")]
+        public void GivenIHaveEnteredTheUserWithThePassword(string user, string password, string database)
+        {
+			settings.Credentials = new MongoCredential[] { MongoCredential.CreateCredential(database, user, password) };
+        }
+        
+        [When(@"I try to connect to ""(.*)"" database")]
+        public void WhenITryToConnect(string database)
+        {
+			client = new MongoClient(settings);
+			try {
+				client.GetDatabase(database);
+			}
+			catch(Exception exc)
+			{
+				//ScenarioContext.Current
+				this.exception = exc;
+			}
+        }
+        
+        [Then(@"there are no errors")]
+        public void ThenThereIsNoErrors()
+        {
+			Assert.True(exception == null); 
+        }
+
+		[Then(@"return an error")]
+		public void ThenReturnAnError()
+		{
+			Assert.True(exception != null); 
+		}
+
+    }
+}
