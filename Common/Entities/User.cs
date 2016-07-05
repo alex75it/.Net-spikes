b24feaaf23b8c4fc828c05bcb6af7ae05057a09f
Alex75 <alessandro.piccione.75@gmail.com>
2016-07-05 21:58:52 +0100

    Add entity User with ID generated by fistName and lastName.

---
+++ Common/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spikes.Common.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string  Email { get; set; }
+
+
+        public User(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+                throw new Exception("firstName and lastName cannot be nyull or empty");
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string Id {
+            get {
+                if (id == null)
+                    id = FirstName.ToLowerInvariant() + "." + LastName.ToLowerInvariant();
+                return id;
+            }
+            set { }
+        }
+
+        private string id;
+    }
+}
