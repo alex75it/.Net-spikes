9d2f66738a0628f3e3645f096f90310b1c1d2af8
Alex75 <alessandro.piccione.75@gmail.com>
2016-09-16 20:31:04 +0100

    Create Rack class

---
+++ Rack/Rack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rack
+{
+    public class Rack : IRack
+    {
+        public void AddBall(int ball)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> Balls()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
