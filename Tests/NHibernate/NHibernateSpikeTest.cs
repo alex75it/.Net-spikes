16d8bf991a4deb83b024f49c1b59feaf3d2a85f4
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-04 23:08:23 +0100

    Add test class

---
+++ Tests/NHibernate/NHibernateSpikeTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spikes.Tests.NHibernate
+{
+    [TestFixture]
+    public class NHibernateSpikeTest
+    {
+        [Test]
+        public void RunQuery_should_ExecuteOnlyOneSqlQuery()
+        {
+            Assert.Fail("not already implemented");
+
+
+        }
+    }
+}
