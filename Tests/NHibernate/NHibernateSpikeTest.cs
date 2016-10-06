5e69eacecd794078357d3737dcce3f5e1632542b
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-06 23:08:06 +0100

    (WIP) Add test with infrastructure for obtaining executed queries.

--- Tests/NHibernate/NHibernateSpikeTest.cs
+++ Tests/NHibernate/NHibernateSpikeTest.cs
@@ -1,27 +1,64 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Should;
 
 namespace Spikes.Tests.NHibernate
 {
-    [TestFixture]
+    [TestFixture, Category("NHibernate")]
     public class NHibernateSpikeTest
     {
         [Test]
         public void RunQuery_should_ExecuteOnlyOneSqlQuery()
-        {
-            Assert.Fail("not already implemented");
+        {            
+            string textToSearch = "Category";
+            DateTime fromTime = DateTime.Now;
+            var queriesBefore = GetSqlQueries(textToSearch, fromTime);
 
-            int numberOfExecutedSelect = GetSqlQueryes();
+            queriesBefore.Count.ShouldEqual(0, "Initial number of queries should be zero.");
+
+            //...
+
+
+            var queriesAfter = GetSqlQueries(textToSearch, fromTime);
+            var newQueries = queriesAfter.Select(q => queriesBefore.Any(qb => qb.Item1 == q.Item1)).ToList();
+
+            newQueries.Count.ShouldEqual(1);
         }
 
-        private int GetSqlQueryes()
+        private IList<Tuple<string, DateTime, string>> GetSqlQueries(string textToSearch, DateTime fromTime)
         {
-            obtain the SQL commands executed
-            throw new NotImplementedException();            
+            IList<Tuple<string, DateTime, string>> queries = new List<Tuple<string, DateTime, string>>();
+            
+            string comandText = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "NHibernate\\Select last queries.sql"))
+                .Replace("{search}", textToSearch).Replace("{time}", fromTime/*.ToUniversalTime*/.ToString("s")); // "o" is ISO format but not accepted by SQL (too long), "s" means sortable and does not set offset time
+
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["spikes"].ConnectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = comandText;
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string handle = reader.GetString(0);
+                    DateTime time = reader.GetDateTime(1);
+                    string text = reader.GetString(2);
+                    int count = (int)reader.GetInt64(3);
+                    queries.Add(Tuple.Create(handle, time, text));
+                }
+            }
+
+            return queries;
         }
     }
 }
---
+++ Tests/NHibernate/Select last queries.sql
@@ -0,0 +1,12 @@
+﻿select 
+	convert(varchar(max), stats.sql_handle, 2) as handle,
+	stats.last_execution_time as time, 
+	text.text, 
+	stats.execution_count as count
+from sys.dm_exec_query_stats as stats 
+	cross apply  sys.dm_exec_sql_text(stats.sql_handle) as text
+where 
+	text.text like '%{search}%' and stats.last_execution_time >= '{time}'
+	and text.text not like '%sys.dm_exec_query_stats%' 
+--order by
+--	stats.last_execution_time desc
