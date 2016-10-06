19af637023bad5273b2773d1721e0c7dcf2b80f3
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-06 23:25:35 +0100

    Add a Sql Database project with Category table.

---
+++ Spikes database/Category.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Category]
+(
+    [Id] INT NOT NULL PRIMARY KEY, 
+    [Code] VARCHAR(10) NOT NULL, 
+    [Name] NVARCHAR(50) NULL, 
+    [CreateDate] DATETIME NOT NULL DEFAULT GetDate()
+)
