a9266ea340a02c315bdda180ad8ff5d165d54c71
Alex75 <alessandro.piccione.75@gmail.com>
2016-10-15 12:04:20 +0100

    Add GeneriscSpike project. Add QuickSpike class.

--- Console Application/IocInstaller.cs
+++ Console Application/IocInstaller.cs
@@ -1,6 +1,7 @@
 ﻿using Ninject;
 using Spikes.Common;
 using Spikes.Couchbase;
+using Spikes.GenericSpike;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +17,9 @@ namespace Spikes.Console
         public static void Initialize()
         {
             kernel = new StandardKernel();
-            kernel.Bind<ISpike>().To<DefaultSpike>();  
+            //kernel.Bind<ISpike>().To<DefaultSpike>();
             //kernel.Bind<ISpike>().To<CouchbaseSpike>();
+            kernel.Bind<ISpike>().To<QuickSpike>();
         }
 
         public static SpikeRunner CreateSpikeRunner()
---
+++ GenericSpike/QuickSpike.cs
@@ -0,0 +1,17 @@
+﻿using Spikes.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spikes.GenericSpike
+{
+    public class QuickSpike : SpikeBase
+    {
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
